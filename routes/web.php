9def63e8922816c9f23cf9fe652f413c91fdf237
yassine el kharouai <yassinekharouai@gmail.com>
2025-03-22 22:30:52 +0000

    secure admin auth

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,3 +20,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+route::get('admin/dashboard',[HomeController::class,'index'])->middleware(['auth','admin']);
